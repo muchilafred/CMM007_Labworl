797079e411fc7c78c51f204a139651729e358081
muchilafred <f.m.muchila@rgu.ac.uk>
2017-03-29 02:39:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,9 +41,9 @@
    
    $password = hash('sha256', $pass); // password hashing using SHA256
   
-   $res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
-   $row=mysql_fetch_array($res);
-   $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
+   $res=mysqli_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
+   $row=mysqli_fetch_array($res);
+   $count = mysqli_num_rows($res); // if uname/pass correct it returns must be 1 row
    
    if( $count == 1 && $row['userPass']==$password ) {
     $_SESSION['user'] = $row['userId'];
